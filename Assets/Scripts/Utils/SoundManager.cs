ce18d38dc96cb3f62ce9de6ce69f5b796ed78593
Andrew Caruso <andrew.caruso03@gmail.com>
2017-03-15 00:22:05 -0400

    Removed Rider plugin, created Timer script

---
+++ Assets/Scripts/Utils/SoundManager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+namespace Utils {
+
+    public enum SoundType {
+        MainMenu, Other, Idk
+    }
+
+    public class SoundManager : MonoBehaviour {
+
+        public static SoundManager Instance;
+
+        // Private constructor since this is a singleton.
+        private SoundManager() { }
+
+        void Awake() {
+            if (Instance == null) {
+                Instance = this;
+            } else if (Instance != this)
+                Destroy(gameObject);
+
+            DontDestroyOnLoad(gameObject);
+        }
+
+        public void PlaySound(SoundType type) {
+
+        }
+    }
+}
