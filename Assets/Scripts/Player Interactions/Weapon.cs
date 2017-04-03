81bff6fa6a14a9c3597dd4e6a64f842a766639e4
Malomer <connorb1234@hotmail.com>
2017-04-03 09:44:00 -0400

    Harpoon
    Spear sprite w/ some basic code

---
+++ Assets/Scripts/Player Interactions/Weapon.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour {
+
+	private int Dmg; //damage or "rage" it inflicts on shark
+	private int BluntDmg; //possibly damage for using the wood end to enrage shark
+	public bool pickedUp = false;
+	public Transform player;
+	private GameObject weapon;
+
+	// Use this for initialization
+	void Start () {
+
+		weapon = this.gameObject;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (pickedUp == true) 
+		{
+			this.transform.parent = player;
+		} 
+		else
+		{
+			this.transform.parent = null; //this becomes cage so that it moves with the cage
+		}
+		if (Input.GetMouseButtonDown(0))
+		{
+			//animate stab
+		}
+
+	}
+}
