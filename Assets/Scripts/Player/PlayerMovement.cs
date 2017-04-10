5338490e8e97c6e8a352c354f237a9e256e93870
Kenneth Mak <kennmak@gmail.com>
2017-04-09 22:01:14 -0400

    Shark script created, undo player movement rehaul, added speed slowing effect

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,66 +1,80 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlayerMovement : MonoBehaviour 
-{
-    public bool moving = true;
-
-    float speed = 5.0f;
-
+public class PlayerMovement : MonoBehaviour {
+	public float floatForce = 2;
+	public float moveSpeed = 6;
+	public float rotSpeed = 10;
+	public float slowingSpeed = 0.2f;
+	public float waveSpeed = 1;
+	float wavePos = 0;
+	public Rigidbody rigB;
 
 	// Use this for initialization
 	void Start () {
-	
-	}
-	
-	void Update () 
-    {
-        if ( moving )
-            Movement();
-
-        MovementCheck();
+		rigB = this.GetComponent<Rigidbody> ();
 	}
 
-    public void SetMoving (bool val)
-    {
-        moving = val;
-    }
+	// Update is called once per frame
+	void Update () {
+		SwimSpeedSlowing ();
+		VerticalInput ();
+		SwimInput ();
 
-    void Movement()
-    {
-        moving = false;
 
-        if ( Input.GetKey(KeyCode.W))
-        {
-            transform.Translate(Vector3.up * speed * Time.deltaTime, Space.World);
-            moving = true;
-        }
+		Waves ();
+	}
 
-        if ( Input.GetKey(KeyCode.S))
-        {
-            transform.Translate(Vector3.down * speed * Time.deltaTime, Space.World);
-            moving = true;
-        }
+	void SwimSpeedSlowing(){
+		rigB.velocity = rigB.velocity * 0.95f;
+	}
 
-        if ( Input.GetKey(KeyCode.A))
-        {
-            transform.Translate(Vector3.left * speed * Time.deltaTime, Space.World);
-            moving = true;
-        }
+	void VerticalInput() {
+		if (Input.GetKey (KeyCode.Space)) {
+			if (rigB.velocity.y <= 2)
+				rigB.velocity += (new Vector3 (0, floatForce, 0));
+		} else if (Input.GetKey (KeyCode.LeftShift)) {
+			if (rigB.velocity.y >= -2)
+				rigB.velocity += (new Vector3 (0, -floatForce, 0));
+		}
+		else {
+			if (rigB.velocity.y <= 0.1 && rigB.velocity.y >= -0.1)
+				rigB.velocity = new Vector3 (rigB.velocity.x, 0, rigB.velocity.z);
+			else if (rigB.velocity.y >= 0)
+				rigB.velocity += (new Vector3 (0, -floatForce, 0));
+			else if (rigB.velocity.y <= 0)
+				rigB.velocity += (new Vector3 (0, floatForce, 0));
+		}
+	}
 
-        if ( Input.GetKey(KeyCode.D))
-        {
-            transform.Translate(Vector3.right * speed * Time.deltaTime, Space.World);
-            moving = true;
-        }
+	void SwimInput() {
+		if (Input.GetKey (KeyCode.W))
+			rigB.velocity += (transform.forward * moveSpeed);
+		else if (Input.GetKey (KeyCode.S))
+			rigB.velocity += (-transform.forward * moveSpeed);
 
-    }
 
-    void MovementCheck()
-    {
-        if (!Input.GetKey(KeyCode.D) && !Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.S) && !Input.GetKey(KeyCode.W))
-            moving = false;
-        else
-            moving = true;
-    }
+		if (Input.GetKey (KeyCode.D))
+			rigB.velocity += (transform.right * moveSpeed);
+		//transform.Rotate (new Vector3(0, rotSpeed, 0) * Time.deltaTime);
+		else if (Input.GetKey (KeyCode.A))
+			rigB.velocity += (-transform.right * moveSpeed);
+		//transform.Rotate (new Vector3(0,-rotSpeed, 0) * Time.deltaTime);
+		SpeedControl ();
+
+
+	}
+
+	void SpeedControl(){
+		if (rigB.velocity.magnitude > moveSpeed) {
+			//Debug.Log (rigB.velocity);
+			rigB.velocity = rigB.velocity.normalized * moveSpeed;
+		}
+	}
+
+
+	void Waves() {
+		wavePos += waveSpeed * Time.deltaTime;
+		transform.Translate(new Vector3(0, Mathf.Sin(wavePos) / 400, 0));
+	}
 }
