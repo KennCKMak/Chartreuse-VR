449b865947de52931ac2ce3675b632da47f5d62e
Pasha-Levin <pavlik_levin@outlook.com>
2017-04-16 12:39:50 -0400

    Harpoon Collision
    Added harpoon collision script
    In scripts folder

---
+++ Assets/Scripts/HarpoonCollide.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HarpoonCollide : MonoBehaviour {
+
+	GameObject harpoon;
+	GameObject creature;
+	private Component script;
+
+	void Start(){
+		script = GetComponent<HarpoonCollide> ();
+	}
+	
+	void OnCollisionEnter(Collision col) {
+		if (col.gameObject.tag == "Creature") {
+			transform.parent = col.transform;
+			Debug.Log ("collision with harpoon");
+
+		}
+	
+}
+}
